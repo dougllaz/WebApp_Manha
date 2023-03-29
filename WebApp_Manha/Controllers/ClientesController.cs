529a37b133c97943b1f339e7aa0cdbc3b0183dd5
Douglas Henrique <Dougllaz@hotmail.com>
2023-03-29 09:08:54 -0300

    Tela de lista de clientes

---
+++ WebApp_Manha/Controllers/ClientesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Manha.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
