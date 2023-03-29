90254bdb2db3cdcc37f5f7b5e2ca98c51f20b0e3
Douglas Henrique <Dougllaz@hotmail.com>
2023-03-29 11:04:01 -0300

    cadastro de erro

--- WebApp_Manha/Controllers/ClientesController.cs
+++ WebApp_Manha/Controllers/ClientesController.cs
@@ -12,5 +12,31 @@ namespace WebApp_Manha.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Cadastro(string Nome, string Telefone) 
+        {
+            if (string.IsNullOrEmpty(Nome))
+            {
+                TempData["Erro"] = "O campo nome não pode estar em branco";
+            }
+
+            if (string.IsNullOrEmpty(Telefone))
+            {
+                TempData["Erro"] = "O campo nome não pode estar em branco";
+            }
+            return View();
+        }
+        public IActionResult Editar(int id)
+        {
+            return View();
+        }
+        public IActionResult Excluir(int id)
+        {
+            return View();
+        }
+        public IActionResult Compras(int id)
+        {
+            return View();
+        }
     }
 }
